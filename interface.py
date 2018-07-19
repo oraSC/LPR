136cd6c7ce1f3cc43d81fb609435ba936c4f1413
oraSC <1301912993@qq.com>
2018-07-19 23:29:34 +0800

    checkbutton of tkinter learning , practice code

--- interface.py
+++ interface.py
@@ -7,13 +7,19 @@ var1 = tk.StringVar()
 #var1.set("1234")    对tkinter变量进行赋值
 la = tk.Label(window,bg = "red",width = 20, height = 2,textvariable = var1)
 la.pack()
-def show_scale_selection(num):
-    var1.set(num)
-
-sc = tk.Scale(window , label = "my scale",orient = tk.HORIZONTAL ,  length = 200, showvalue = 0 ,from_ = 0 ,to = 10 ,
-              tickinterval = 2,resolution = 0.01,command = show_scale_selection)
-
-sc.pack()
-
+def show_checkbutton():
+    if (var2.get() == 1)and(var3.get() == 0) :
+        var1.set("ture")
+    if (var2.get() == 0)and(var3.get() == 1) :
+        var1.set("false")
+    if ((var2.get() == 0) and (var3.get() == 0))or((var2.get() == 1) and (var3.get() == 1)):
+        var1.set("uncertain")
+#创建tkinter特有变量Int
+var2 = tk.IntVar()
+var3 = tk.IntVar()
+c1 = tk.Checkbutton(window , text = "true" , variable = var2 , onvalue = 1 ,offvalue = 0 ,command = show_checkbutton)
+c2 = tk.Checkbutton(window , text = "false" , variable = var3 , onvalue = 1 ,offvalue = 0 ,command = show_checkbutton)
+c1.pack()
+c2.pack()
 
 window.mainloop()
