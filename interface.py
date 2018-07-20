9ca0917fb536363d099a872e47a199d088292614
oraSC <1301912993@qq.com>
2018-07-20 12:46:00 +0800

    test

--- interface.py
+++ interface.py
@@ -1,26 +1,22 @@
-import tkinter as tk
-window = tk.Tk()
-window.title("my_window")
-window.geometry("500x500")
+class employee:
+    #所有employee公有元素
+    employee_num = 0
+    #类的构造函数，self代表假定的实类
+    def __init__(self,name,salary):
+        self.name = name
+        self.salary = salary
+        employee.employee_num += 1
+        self.NO = employee.employee_num
+    #类的析构函数
+    # def __del__(self):
+    #     print(self.name,"have been fired")
+    #     employee.employee_num -= 1
+    def displayemployee(self):
+        print("name:",self.name,"salary:",self.salary,"NO.",self.NO)
 
-var = tk.IntVar()
-var.set(0)
-def fun():
-    var.set(var.get() +1)
+em1 = employee("陈志伟","1k")
+em2 = employee("路痴不橙","2k")
 
-#创建主菜单
-menubar = tk.Menu(window)
-#创建子菜单
-filemenu = tk.Menu(menubar ,tearoff = 0)
-#添加子菜单头
-menubar.add_cascade(label = "File",menu = filemenu )
-#添加子菜单列表(功能)
-filemenu.add_command(label = "new project" , command = fun)
-filemenu.add_command(label = "open",command = fun)
-filemenu.add_command(label = "save" , command = fun)
-
-
-
-
-window.config(menu = menubar)
-window.mainloop()
+em1.displayemployee()
+em2.displayemployee()
+del em2
